f52f66ec5b657a17fd771e1c664107dd4042f624
ramdas <ramdasinfoinzenjer@gmail.com>
2018-01-08 17:38:18 +0530

    Initial commit

---
+++ app/src/main/java/com/inzenjer/automaticdoorlock/Login_login.java
@@ -0,0 +1,21 @@
+package com.inzenjer.automaticdoorlock;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Login_login extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState){
+        View rootView = inflater.inflate(R.layout.activity_login_login, container, false);
+
+        return rootView;
+
+    }
+}
---
+++ app/src/main/java/com/inzenjer/automaticdoorlock/Splash.java
@@ -0,0 +1,25 @@
+package com.inzenjer.automaticdoorlock;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Splash extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(Splash.this,MainActivity.class));
+                //Do something after 100ms
+            }
+        }, 3000);
+    }
+}
